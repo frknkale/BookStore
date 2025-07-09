2121a7fe16823b67710ceccfe84b674f02c25c6c
Furkan <frknkale556@gmail.com>
2025-07-09 20:07:06 +0300

    App-6

--- bookstore/connectDB.php
+++ bookstore/connectDB.php
@@ -1,4 +1,4 @@
 <?php
 include 'config.php';
-$pdo=new PDO('mysql:host=$mysql_host;port=3306;dbname=BookStore',$mysql_username, $mysql_password);
+$pdo=new PDO("mysql:host=$mysql_host;port=3306;dbname=BookStore",$mysql_username, $mysql_password);
 ?>
