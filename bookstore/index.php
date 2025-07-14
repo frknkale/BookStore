0eb1186d714cc94fd305352489f0c3c0ca66620b
emreburakbakdemir <emreburakbakdemir38@gmail.com>
2025-07-14 15:57:21 +0300

    test

--- bookstore/index.php
+++ bookstore/index.php
@@ -1,4 +1,4 @@
-<html>
+<htm>
 <meta http-equiv="Content-Type" content="text/html; charset=utf8"/>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="style.css">
