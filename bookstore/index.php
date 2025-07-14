6ad13cc9fc882d145dfd0fa51cf996d54f498f4d
emreburakbakdemir <emreburakbakdemir38@gmail.com>
2025-07-14 16:02:40 +0300

    test

--- bookstore/index.php
+++ bookstore/index.php
@@ -8,7 +8,7 @@ include 'config.php';
 session_start();
 
 // Function to create database connection
-function getConnectionn() {
+function getConnectionnnn() {
     global $mysql_host, $mysql_username, $mysql_password;
     $conn = new mysqli($mysql_host, $mysql_username, $mysql_password);
     if ($conn->connect_error) {
