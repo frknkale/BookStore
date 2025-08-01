d39d5b3633a7a0cba234507951f0798a26c58441
Furkan <frknkale@hotmail.com>
2025-08-01 11:49:40 +0300

    test

--- bookstore/login.php
+++ bookstore/login.php
@@ -8,13 +8,13 @@
 </header>
 <blockquote>
 <div class="container">
-<center><h1>Login</h1></center>
+<center><h1>LoginJ</h1></center>
 <form action="checklogin.php" method="post">
     Usernamee:<br><input type="text" name="username"/>
     <br><br>
     Password:<br><input type="password" name="pwd" />
     <br><br>
-    <input class="button" type="submit" value="LoginJ"/>
+    <input class="button" type="submit" value="Login"/>
     <input class="button" type="button" name="cancel" value="Cancel" onClick="window.location='index.php';" />
 </form>
 </div>
